b3d8c8f771424968c556fc842c22a4b298110d8d
The Blockchainer <132522565+DVO1111@users.noreply.github.com>
2025-10-15 10:43:18 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,15 +10,8 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    react(), 
+    react(),
     mode === "development" && componentTagger(),
-    nodePolyfills({
-      include: ['buffer', 'process'],
-      globals: {
-        Buffer: true,
-        process: true,
-      },
-    })
   ].filter(Boolean),
   resolve: {
     alias: {
